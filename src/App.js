53ff063375fe80dc1d66d58ca1ef41718ce4145e
araz0515 <arazkubtanjian@gmail.com>
2023-09-10 01:37:15 -0400

    Created landing page hero + added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      {" "}
+      <Navbar />
+      <header className="App-header">
+        <h3 className="intro">Hi👋 My name is</h3>
+        <h1>Araz Kubtanjian</h1>
+        <h3>Front-end Developer based in Montreal, Quebec</h3>
+        <h3 className="about-me">
+          Creating functional websites from conceptual ideas is my true vocation
+        </h3>
+      </header>
     </div>
   );
 }
