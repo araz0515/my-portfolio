ae8876f2a0277db6c1f5e77206f9d5e848c10ad9
araz0515 <arazkubtanjian@gmail.com>
2023-09-10 18:04:04 -0400

    Added about me text

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,14 +4,22 @@ import myPhoto from "./myPhoto.jpg";
 
 export default function AboutMe() {
   return (
-    <div className="AboutMe container">
+    <div className="AboutMe">
       <div className="row">
         <div className="col-md-6">
-          <img src={myPhoto} alt="this is me" />
+          <img src={myPhoto} alt="this is me" className="img-fluid" />
         </div>
         <div className="col-md-6">
           <h1>About Me</h1>
-          <p>My name is Araz Kubtanjian. </p>
+          <p>
+            My name is Araz Kubtanjian. I'm a Junior Front-end Developer who's
+            passionate about technology. I have an adverse educational
+            background, where I've studied in the domains of science as well as
+            real estate. However I was never devoted to any career path until
+            now. Software development has captured my heart and I'm now a
+            devoted aspiring front-end developer hoping to build a career and
+            improve my coding skills along the way.{" "}
+          </p>
         </div>
       </div>
     </div>
